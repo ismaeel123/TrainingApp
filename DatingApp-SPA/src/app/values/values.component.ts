86af99549cb1d8551844ce64fc9771b1a4ca6fa0
ismaeel123 <37120315+ismaeel123@users.noreply.github.com>
2020-11-16 00:04:19 +0200

    initial Commit

---
+++ DatingApp-SPA/src/app/values/values.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-values',
+  templateUrl: './values.component.html',
+  styleUrls: ['./values.component.css']
+})
+export class ValuesComponent implements OnInit {
+
+  values:any;
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues(){
+    this.http.get("http://localhost:5000/weatherforecast").subscribe(response=>{
+        this.values=response;
+    },error =>{
+      console.log(error);
+    })
+  }
+
+}
---
+++ DatingApp.api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _Context;
+
+        public WeatherForecastController(DataContext Context)
+        {
+            _Context=Context;
+        }
+
+        /*
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        */
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values=await _Context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value=await _Context.Values.FirstOrDefaultAsync(v=>v.Id==id);
+            return Ok(value);
+        }
+    }
+}
